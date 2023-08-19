fadc6257c6f9bdc46ea6558fed1354937adbe75a
tylermcnair1 <142367783+tylermcnair1@users.noreply.github.com>
2023-08-19 15:00:39 -0500

    continued practice

--- consolelogexamples01.js
+++ consolelogexamples01.js
@@ -4,3 +4,11 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+
+console.log("what, do , commas do")
+
+console.log ('adding a space does not matter')
+console.log ('add a "+" sign combines the words')
+
+/*i continue to have issues submitting using git push. 
+i will continue to try, but i have also used the upload file option to show my work on github*/
